08110436fd23dc0540813d7951e62a8c2bebe320
Amit Pandey <amitpandey675@gmail.com>
2017-08-03 15:47:40 +0530

    Add files via upload

---
+++ includes/dbconnection.php
@@ -0,0 +1,4 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+$con= new mysqli('localhost','root','','election') or die("Could not connect to mysql - ".mysqli_error($con));
+?>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div style="height: 50px; background-color: black; color: white; padding-top: 20px;">
+    <div class="container-fluid">
+        <div class="row" style="font-variant: small-caps">
+            <div class="col-md-6" style="text-align: left;">Submitted To - Prof. Abhay Agarwal</div>
+            <div class="col-md-6" style="text-align: right;">Submitted By - Amit Pandey, Atul Gupta, Madhav Gopal Sharma</div>
+        </div>
+    </div>
+</div>
